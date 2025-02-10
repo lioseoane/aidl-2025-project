1c850f32a5a9c3ea21a65aec3d7fc5ec7ff117d4
lioseoane <60878041+lioseoane@users.noreply.github.com>
2025-02-10 11:17:17 +0100

    fix

--- src/data/load_workout_data.py
+++ src/data/load_workout_data.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
 
-def load_workout_data(image_dir="workout_dataset\images", annotation_dir="workout_dataset\labels"):
+def load_workout_data(image_dir="workout_dataset/images", annotation_dir="workout_dataset/labels"):
     # Hardcoded paths
     image_dir = image_dir
     annotation_dir = annotation_dir
@@ -29,6 +29,10 @@ def load_workout_data(image_dir="workout_dataset\images", annotation_dir="workou
             if img_filename.endswith(('.jpg', '.jpeg', '.png')):  # Filter image files
                 img_path = os.path.join(class_img_dir, img_filename)
                 txt_path = os.path.join(class_annotation_dir, os.path.splitext(img_filename)[0] + ".txt")
+
+                # Debug print statements
+                print(f"Checking image: {img_path}")
+                print(f"Expected annotation file: {txt_path}")
 
                 # Check if the annotation file exists
                 if not os.path.exists(txt_path):
@@ -64,7 +68,6 @@ def load_workout_data(image_dir="workout_dataset\images", annotation_dir="workou
                     bbox = [x_min, y_min, x_max, y_max]
 
                     # Reshape keypoints into [x, y] (ignoring visibility)
-                    # keypoints = [[values[i], values[i+1]] for i in range(5, len(values), 2)]
                     keypoints = [[values[i], values[i+1], values[i+2]] for i in range(5, len(values)-1, 3)]
 
                     if len(keypoints) != 17:  # Exclude images that do not have 17 keypoints
@@ -133,17 +136,17 @@ def plot_image_with_annotations(image_path, bbox, keypoints, class_name):
 
 if __name__ == "__main__":
     # PROJECT_DIR = os.path.abspath(os.path.join(os.getcwd(), '..'))
-    PROJECT_DIR = "/home/andres/Escritorio/aidl-2025-project"
+    PROJECT_DIR = "C:/Users/super/Desktop/AIHL_project/aidl-2025-project"
     image_dir = os.path.join(PROJECT_DIR, 'workout_dataset/images')
     annotation_dir = os.path.join(PROJECT_DIR, 'workout_dataset/new_labels')
     keypoints_array, images_array, bounding_boxes_array, classes_array = load_workout_data(image_dir=image_dir, annotation_dir=annotation_dir)
     print(len(keypoints_array), len(images_array), len(bounding_boxes_array), len(classes_array))
 
     # Get the first image and its annotations
-    image_path = images_array[0]
-    bbox = bounding_boxes_array[0]
-    keypoints = keypoints_array[0]
-    class_name = classes_array[0]
+    image_path = images_array[10]
+    bbox = bounding_boxes_array[10]
+    keypoints = keypoints_array[10]
+    class_name = classes_array[10]
 
     # Plot the first image with its bounding box and keypoints
     plot_image_with_annotations(image_path, bbox, keypoints, class_name)
